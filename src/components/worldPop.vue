614c50bad1ed5660c359fb0695e8bd75a5ec780b
Nahd92 <dhan.marcellus@hotmail.com>
2021-09-13 13:27:04 +0200

    first commit

---
+++ src/components/worldPop.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="worldPop">
+    <div v-for="pop in populations" v-bind:key="pop.id">
+      <div class="country">Country: {{ pop.countryName }}</div>
+      <div class="country">Population: {{ pop.value }}</div>
+      <br />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "worldPop",
+  data: () => ({
+    populations: [],
+  }),
+  mounted() {
+    fetch("https://countriesnow.space/api/v0.1/countries/population")
+      .then((response) => response.json())
+      .then((data) => {
+        for (let i = 1; i < data.data.length; i++) {
+          const countryName = data.data[i].country;
+          const countriespopulationCounts = data.data[i].populationCounts;
+          countriespopulationCounts.forEach((population) => {
+            if (population.year == "2018") {
+              const value = population.value;
+              this.populations.push({ countryName, value });
+            }
+          });
+        }
+      });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
